1af0cded25bd6cfffc106a317ee5fb01e20328ec
MaxDistructo <micanjvollan@gmail.com>
2019-01-23 20:40:00 +0000

    Almost finish new Perms object. (Forgot to fix imports)

--- src/main/java/maxdistructo/discord/core/jda/impl/Perms.kt
+++ src/main/java/maxdistructo/discord/core/jda/impl/Perms.kt
@@ -1,6 +1,72 @@
 package maxdistructo.discord.core.jda.impl
 
+import maxdistructo.discord.core.jda.obj.IPerms
 
-class Perms(fileSuffix : String, ) : IPerms{
+class Perms(fileSuffix : String, guild : Guild) : IPerms{
+    val jsonFile : JSONObject
+    val guildId : Long
+    
+    constructor(guild : Guild){
+        jsonFile = Utils.readJSONFromFile("" + guild.idLong + "_perms.json")
+        guildId = guild.idLong
+    }
+    init{
+        jsonFile = Utils.readJSONFromFile("" + guild.idLong + fileSuffix)
+        guildId = guild.idLong
+    }
+
+    val mods = jsonFile.getJSONArray("mods")
+    val admins = jsonFile.getJSONArray("admins")
+    val owner = guild.getOwner.user.idLong
+
+    //Default Permission Check Function
+    fun checkMod(id : Long) : Boolean{
+        return mods.contains(id) || admins.contains(id) || id == owner
+    }
+    fun checkAdmin(id : Long) : Boolean{
+        return admins.contains(id) || id == owner
+    }
+    fun checkOwner(id : Long) : Boolean{
+        return id == owner
+    }
+
+    //Variants of Default Permission Check Function
+    fun checkMod(user : User) : Boolean{
+        return checkMod(user.idLong)
+    }
+    fun checkMod(member : Member) : Boolean{
+        return checkMod(member.user.idLong)
+    }
+    fun checkMod(message : Message) : Boolean{
+        return checkMod(message.author.idLong)
+    }
+    fun checkMod(idString : String) : Boolean{
+        return checkMod(Utils.convertToLong(idString))
+    }
+    fun checkAdmin(user : User) : Boolean{
+        return checkAdmin(user.idLong)
+    }
+    fun checkAdmin(member : Member) : Boolean{
+        return checkAdmin(member.user.idLong)
+    }
+    fun checkAdmin(message : Message) : Boolean{
+        return checkAdmin(message.author.idLong)
+    }
+    fun checkAdmin(idString : String) : Boolean{
+        return checkAdmin(Utils.convertToLong(idString))
+    }
+    fun checkOwner(user : User) : Boolean{
+        return checkOwner(user.idLong)
+    }
+    fun checkOwner(member : Member) : Boolean{
+        return checkOwner(member.user.idLong)
+    }
+    fun checkOwner(message : Message) : Boolean{
+        return checkOwner(message.author.idLong)
+    }
+    fun checkOwner(idString : String) : Boolean{
+        return checkOwner(Utils.convertToLong(idString))
+    }
+    
 
 }
