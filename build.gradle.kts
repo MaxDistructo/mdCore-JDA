1f8f40f4aa9422a8adf6177df4c6dd19508fc069
MaxDistructo <mvollan01@outlook.com>
2021-07-23 23:52:24 +0000

    Update most deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 }
 plugins {
   java
   idea
-    `java-library`
-    maven
-
-  kotlin("jvm") version "1.4.30"
+  `java-library`
+  maven
+  kotlin("jvm") version "1.5.21"
     //id "com.github.ben-manes.versions" version "0.20.0"
     //id 'com.sedmelluq.jdaction' version '1.0.2'
 }
@@ -24,21 +23,20 @@ group = "com.gitlab.MaxDistructo"
 repositories {
   jcenter()
   mavenCentral()
-  maven{
-    name = "JDA Bintray"
-    url = uri("https://dl.bintray.com/dv8fromtheworld/maven")
-  }
+  maven("https://m2.dv8tion.net/releases")
 }
+
+//val compile by configurations.creating
 
 // In this section you declare the dependencies for your production and test code
 dependencies {
-  compile (group= "org.jetbrains.kotlin", name= "kotlin-stdlib", version="1.4.30")
-  compile (group= "org.jetbrains.kotlinx", name= "kotlinx-coroutines-core", version="1.3.7")
+  compile (group= "org.jetbrains.kotlin", name= "kotlin-stdlib", version="1.5.21")
+  compile (group= "org.jetbrains.kotlinx", name= "kotlinx-coroutines-core", version="1.5.1")
   compile (group= "org.json", name= "json", version="20200518")
   compile (group= "commons-io", name= "commons-io", version="2.7")
   compile (group= "ch.qos.logback", name= "logback-classic", version="1.2.3")
-  compile (group = "net.dv8tion", name= "JDA", version = "4.2.0_228")
-  compile (group = "club.minnced", name= "discord-webhooks", version= "0.5.5-rc")
+  compile (group = "net.dv8tion", name= "JDA", version = "4.3.0_277")
+  compile (group = "club.minnced", name= "discord-webhooks", version= "0.5.7")
   compile (group = "com.jagrosh", name = "jda-utilities", version = "3.0.4")
 }
 
